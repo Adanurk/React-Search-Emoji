5c07b9ecde07772179285b5332cb8e03b609ca53
Adanurk <gumussoykbr@gmail.com>
2022-08-16 23:59:15 +0200

    in progress

--- src/components/EmojisContainer.js
+++ src/components/EmojisContainer.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import emojis from "../json/emoji.json"
 import EmojiItem from './EmojiItem'
+import { useState } from 'react';
 
 export default function EmojisContainer() {
+  const [emos, setEmos] = useState(emojis);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    let key = e.target.value.toLowerCase();
+    let updatedList = emos.filter((item)=>{
+      if(key === ""){
+        return emos
+      }else{
+        return item.keywords.includes(key)
+      }
+    })
+    console.log(updatedList);
+    return setEmos(updatedList)
+  }
+
   return (
     <div style={{display:"flex", flexDirection:"column", alignItems:"center", padding:"10px"}}>
-        <input type="text" name='search' style={{fontSize:"20px", borderRadius:"10px", width:"50%"}}/>
+        <input type="text" name='search' style={{fontSize:"20px", borderRadius:"10px", width:"50%"}} onChange={handleChange}/>
         <div className='container' style={{display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
-            {emojis.map(emojis => {
-                return (<EmojiItem symbol={emojis.symbol} />)
+            {emos.map(emos => {
+                return (<EmojiItem symbol={emos.symbol} />)
             })}
 
         </div>
